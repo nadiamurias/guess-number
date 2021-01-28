4c78e9274840438a8f26bb43a1f8714d814dc43a
Nadia Murias <nadiamurias@gmail.com>
2021-01-28 10:16:56 +0100

    Add conditionals to number

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const inputElement = document.querySelector(".js-number");
 const buttonElement = document.querySelector(".js-button");
 const randomNumber = getRandomNumber(100);
+const trackElement = document.querySelector(".js-track");
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -9,8 +10,21 @@ function getRandomNumber(max) {
 console.log(randomNumber);
 
 function handleCheckNumber() {
-  const inputValue = inputElement.nodeValue;
+  const inputValue = inputElement.value;
   console.log(inputValue);
+  if (inputValue > 100) {
+    trackElement.value = "El número debe estar entre 1 y 100";
+    console.log(trackElement.value);
+  } else if (inputValue > randomNumber) {
+    trackElement.value = "Demasiado alto";
+    console.log(trackElement.value);
+  } else if (inputValue < randomNumber) {
+    trackElement.value = "Demasiado bajo";
+    console.log(trackElement.value);
+  } else if (inputValue == randomNumber) {
+    trackElement.value = "Has ganado campeona!!!";
+    console.log(trackElement.value);
+  }
 }
 
 buttonElement.addEventListener("click", handleCheckNumber);
