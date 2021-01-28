1f945b35aaa34a334aeb26aef35657d29c98aa53
Nadia Murias <nadiamurias@gmail.com>
2021-01-28 09:49:37 +0100

    Add random number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+const inputElement = document.querySelector(".js-number");
+const buttonElement = document.querySelector(".js-button");
+const randomNumber = getRandomNumber(100);
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+console.log(randomNumber);
+
+function handleCheckNumber() {
+  const inputValue = inputElement.nodeValue;
+  console.log(inputValue);
+}
+
+buttonElement.addEventListener("click", handleCheckNumber);
